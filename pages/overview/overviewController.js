940d9b8e47f12ba94469390c420f97a98f3234fb
Hung <quanghungbui1@gmail.com>
2021-04-14 10:40:17 +0200

    aktueller Stand - dummy - overview (Überblicksseite)

---
+++ pages/overview/overviewController.js
@@ -0,0 +1,374 @@
+﻿// controller for page: home
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/appSettings.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+/// <reference path="~/www/lib/convey/scripts/appbar.js" />
+/// <reference path="~/www/lib/convey/scripts/pageController.js" />
+/// <reference path="~/www/scripts/generalData.js" />
+/// <reference path="~/www/pages/overview/overviewService.js" />
+
+(function () {
+    "use strict";
+
+    var nav = WinJS.Navigation;
+
+    WinJS.Namespace.define("Overview", {
+        Controller: WinJS.Class.derive(Application.Controller, function Controller(pageElement, commandList) {
+            Log.call(Log.l.trace, "Overview.Controller.");
+
+            // ListView control
+            var listView = pageElement.querySelector("#overview.listview");
+
+            Application.RecordsetController.apply(this, [pageElement, {
+                eventId: AppData.getRecordId("Veranstaltung"),
+                dataEvent: {},
+                dataText: {},
+                dataMedien: {},
+                count: 0
+            }, commandList, false, Overview.eventView, null, listView]);
+
+
+            if (Application.query && Application.query.eventID) {
+                AppData.setRecordId("Veranstaltung", Application.query.eventID);
+            }
+
+            /*Application.Controller.apply(this, [pageElement, {
+                eventId: AppData.getRecordId("Veranstaltung"),
+                dataEvent: {},
+                dataText: {},
+                dataMedien: {}
+            }, commandList]);*/
+
+            var that = this;
+
+            this.dispose = function () {
+                if (listView && listView.winControl) {
+                    listView.winControl.itemDataSource = null;
+                }
+            }
+
+            var progress = null;
+            var counter = null;
+            var layout = null;
+
+            if (listView && listView.winControl) {
+                layout = new Application.OverviewLayout.EventsLayout;
+                listView.winControl.layout = {
+                    type: layout,
+                    orientation: WinJS.UI.Orientation.vertical
+                };
+            }
+
+            var maxLeadingPages = 0;
+            var maxTrailingPages = 0;
+
+            var setDataEvent = function (newDataEvent) {
+                Log.call(Log.l.trace, "Event.Controller.");
+                var prevNotifyModified = AppBar.notifyModified;
+                AppBar.notifyModified = false;
+                that.binding.dataEvent = newDataEvent;
+                // convert Startdatum 
+                that.binding.dataEvent.dateBegin = getDateObject(newDataEvent.Startdatum);
+                if (AppData._persistentStates.registerData.dateBegin !== that.binding.dataEvent.dateBegin) {
+                    AppData._persistentStates.registerData.dateBegin = that.binding.dataEvent.dateBegin;
+                    Application.pageframe.savePersistentStates();
+                }
+                AppBar.modified = false;
+                if (newDataEvent.VeranstaltungVIEWID && Application.query && window.history) {
+                    var state = {};
+                    var title = "";
+                    Application.query.eventID = newDataEvent.VeranstaltungVIEWID;
+                    var location = window.location.href.split("?")[0] + "?" + createQueryStringFromParameters(Application.query);
+                    window.history.pushState(state, title, location);
+                }
+                AppBar.notifyModified = prevNotifyModified;
+                AppBar.triggerDisableHandlers();
+                Log.ret(Log.l.trace);
+            };
+            this.setDataEvent = setDataEvent;
+
+            var setDataText = function (results) {
+                Log.call(Log.l.trace, "Event.Controller.");
+                var newDataText = {};
+                for (var i = 0; i < results.length; i++) {
+                    var row = results[i];
+                    if (row.LabelTitle) {
+                        newDataText[row.LabelTitle] = row.Title ? row.Title : "";
+                    }
+                    if (row.LabelDescription) {
+                        newDataText[row.LabelDescription] = row.Description ? row.Description : "";
+                    }
+                    if (row.LabelMainTitle) {
+                        newDataText[row.LabelMainTitle] = row.MainTitle ? row.MainTitle : "";
+                    }
+                    if (row.LabelSubTitle) {
+                        newDataText[row.LabelSubTitle] = row.SubTitle ? row.SubTitle : "";
+                    }
+                    if (row.LabelSummary) {
+                        newDataText[row.LabelSummary] = row.Summary ? row.Summary : "";
+                    }
+                    if (row.LabelBody) {
+                        newDataText[row.LabelBody] = row.Body ? row.Body : "";
+                    }
+                }
+                that.binding.dataText = newDataText;
+                Log.ret(Log.l.trace);
+            }
+            this.setDataText = setDataText;
+
+            var setDataMedien = function (results) {
+                Log.call(Log.l.trace, "Event.Controller.");
+                var newDataMedien = {};
+                for (var i = 0; i < results.length; i++) {
+                    var row = results[i];
+                    if (row.LabelTitle) {
+                        var docContent = row.DocContentDOCCNT1
+                            ? row.DocContentDOCCNT1
+                            : row.DocContentDOCCNT1;
+                        if (docContent) {
+                            var sub = docContent.search("\r\n\r\n");
+                            if (sub >= 0) {
+                                var data = docContent.substr(sub + 4);
+                                if (data && data !== "null") {
+                                    row.DocContentDOCCNT1 = "data:image/jpeg;base64," + data;
+                                } else {
+                                    row.DocContentDOCCNT1 = "";
+                                }
+                            } else {
+                                row.DocContentDOCCNT1 = "";
+                            }
+                        } else {
+                            row.DocContentDOCCNT1 = "";
+                        }
+                        newDataMedien[row.LabelTitle] = row.DocContentDOCCNT1 ? row.DocContentDOCCNT1 : "";
+                    }
+                }
+                that.binding.dataMedien = newDataMedien;
+                Log.ret(Log.l.trace);
+            }
+            this.setDataMedien = setDataMedien;
+
+            // define handlers
+            this.eventHandlers = {
+                clickBack: function (event) {
+                    Log.call(Log.l.trace, "Event.Controller.");
+                    if (WinJS.Navigation.canGoBack === true) {
+                        WinJS.Navigation.back(1).done( /* Your success and error handlers */);
+                    }
+                    Log.ret(Log.l.trace);
+                },
+                clickOk: function (event) {
+                    Log.call(Log.l.trace, "Register.Controller.");
+                    that.saveData(function (response) {
+                        // called asynchronously if ok
+                    }, function (errorResponse) {
+                        // called asynchronously on error
+                    });
+                    Log.ret(Log.l.trace);
+                },
+                onItemInvoked: function (eventInfo) {
+                    Log.call(Log.l.trace, "Events.Controller.");
+                    if (eventInfo && eventInfo.detail && that.records) {
+                        Log.print(Log.l.trace, "itemIndex=" + eventInfo.detail.itemIndex);
+                        var item = that.records.getAt(eventInfo.detail.itemIndex);
+                        if (item && item.VeranstaltungVIEWID) {
+                            if (Application.query) {
+                                Application.query.eventID = item.VeranstaltungVIEWID;
+                            } else {
+                                AppData.setRecordId("Veranstaltung", item.VeranstaltungVIEWID);
+                            }
+                            Application.navigateById("event", eventInfo);
+                        }
+                    }
+                    Log.ret(Log.l.trace);
+                },
+                onLoadingStateChanged: function (eventInfo) {
+                    Log.call(Log.l.trace, "Events.Controller.");
+                    if (listView && listView.winControl) {
+                        Log.print(Log.l.trace, "loadingState=" + listView.winControl.loadingState);
+                        // single list selection
+                        if (listView.winControl.selectionMode !== WinJS.UI.SelectionMode.single) {
+                            listView.winControl.selectionMode = WinJS.UI.SelectionMode.single;
+                        }
+                        // direct selection on each tap
+                        if (listView.winControl.tapBehavior !== WinJS.UI.TapBehavior.directSelect) {
+                            listView.winControl.tapBehavior = WinJS.UI.TapBehavior.directSelect;
+                        }
+                        // Double the size of the buffers on both sides
+                        if (!maxLeadingPages) {
+                            maxLeadingPages = listView.winControl.maxLeadingPages * 4;
+                            listView.winControl.maxLeadingPages = maxLeadingPages;
+                        }
+                        if (!maxTrailingPages) {
+                            maxTrailingPages = listView.winControl.maxTrailingPages * 4;
+                            listView.winControl.maxTrailingPages = maxTrailingPages;
+                        }
+                        if (listView.winControl.loadingState === "itemsLoading") {
+                        } else if (listView.winControl.loadingState === "complete") {
+                            if (that.loading) {
+                                progress = listView.querySelector(".list-footer .progress");
+                                counter = listView.querySelector(".list-footer .counter");
+                                if (progress && progress.style) {
+                                    progress.style.display = "none";
+                                }
+                                if (counter && counter.style) {
+                                    counter.style.display = "inline";
+                                }
+                                that.loading = false;
+                            }
+                        }
+                    }
+                    Log.ret(Log.l.trace);
+                },
+                onHeaderVisibilityChanged: function (eventInfo) {
+                    Log.call(Log.l.trace, "Events.Controller.");
+                    if (eventInfo && eventInfo.detail) {
+                        var visible = eventInfo.detail.visible;
+                        if (visible) {
+                        }
+                    }
+                    Log.ret(Log.l.trace);
+                },
+                onFooterVisibilityChanged: function (eventInfo) {
+                    Log.call(Log.l.trace, "Events.Controller.");
+                    if (eventInfo && eventInfo.detail) {
+                        progress = listView.querySelector(".list-footer .progress");
+                        counter = listView.querySelector(".list-footer .counter");
+                        var visible = eventInfo.detail.visible;
+                        if (visible && that.nextUrl) {
+                            that.loading = true;
+                            if (progress && progress.style) {
+                                progress.style.display = "inline";
+                            }
+                            if (counter && counter.style) {
+                                counter.style.display = "none";
+                            }
+                            that.loadNext(function (json) {
+                                // this callback will be called asynchronously
+                                // when the response is available
+                                Log.print(Log.l.trace, "loadNext: success!");
+                            }, function (errorResponse) {
+                                // called asynchronously if an error occurs
+                                // or server returns response with an error status.
+                                AppData.setErrorMsg(that.binding, errorResponse);
+                                if (progress && progress.style) {
+                                    progress.style.display = "none";
+                                }
+                                if (counter && counter.style) {
+                                    counter.style.display = "inline";
+                                }
+                            });
+                        } else {
+                            if (progress && progress.style) {
+                                progress.style.display = "none";
+                            }
+                            if (counter && counter.style) {
+                                counter.style.display = "inline";
+                            }
+                            that.loading = false;
+                        }
+                    }
+                    Log.ret(Log.l.trace);
+                }
+            };
+
+            // page command disable handler
+            this.disableHandlers = {
+                clickBack: function () {
+                    if (WinJS.Navigation.canGoBack === true) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            };
+
+            var loadTextData = function () {
+                Log.call(Log.l.trace, "Event.Controller.");
+                AppData.setErrorMsg(that.binding);
+                var ret = new WinJS.Promise.as().then(function () {
+                    if (that.binding.eventId) {
+                        //load of format relation record data
+                        Log.print(Log.l.trace, "calling select eventView...");
+                        return Overview.eventView.select(function (json) {
+                            AppData.setErrorMsg(that.binding);
+                            Log.print(Log.l.trace, "eventView: success!");
+                            if (json && json.d) {
+                                // now always edit!
+                                that.setDataEvent(json.d);
+                            }
+                        }, function (errorResponse) {
+                            AppData.setErrorMsg(that.binding, errorResponse);
+                        }, that.binding.eventId);
+                    } else {
+                        return WinJS.Promise.as();
+                    }
+                }).then(function () {
+                    //if (that.binding.eventId) {
+                    //load of format relation record data
+                    Log.print(Log.l.trace, "calling select textView...");
+                    return Overview.textView.select(function (json) {
+                        AppData.setErrorMsg(that.binding);
+                        Log.print(Log.l.trace, "labelView: success!");
+                        if (json && json.d) {
+                            // now always edit!
+                            that.setDataText(json.d.results);
+                        }
+                    }, function (errorResponse) {
+                        AppData.setErrorMsg(that.binding, errorResponse);
+                    }, null);
+                }).then(function () {
+                    if (that.binding.eventId) {
+                        //load of format relation record data
+                        Log.print(Log.l.trace, "calling select eventView...");
+                        return Overview.medienView.select(function (json) {
+                            AppData.setErrorMsg(that.binding);
+                            Log.print(Log.l.trace, "labelView: success!");
+                            if (json && json.d) {
+                                // now always edit!
+                                that.setDataMedien(json.d.results);
+                            }
+                        }, function (errorResponse) {
+                            AppData.setErrorMsg(that.binding, errorResponse);
+                        }, that.binding.eventId);
+                    } else {
+                        return WinJS.Promise.as();
+                    }
+                }).then(function () {
+                        AppBar.notifyModified = true;
+                        return WinJS.Promise.as();
+                    });
+                Log.ret(Log.l.trace);
+                return ret;
+            };
+            this.loadTextData = loadTextData;
+
+            var saveData = function (complete, error) {
+                Log.call(Log.l.trace, "Register.Controller.");
+
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+            this.saveData = saveData;
+
+            // add ListView event handler
+            if (listView) {
+                this.addRemovableEventListener(listView, "loadingstatechanged", this.eventHandlers.onLoadingStateChanged.bind(this));
+                this.addRemovableEventListener(listView, "footervisibilitychanged", this.eventHandlers.onFooterVisibilityChanged.bind(this));
+                this.addRemovableEventListener(listView, "headervisibilitychanged", this.eventHandlers.onHeaderVisibilityChanged.bind(this));
+                this.addRemovableEventListener(listView, "iteminvoked", this.eventHandlers.onItemInvoked.bind(this));
+            }
+
+            // finally, load the data
+            that.processAll().then(function () {
+                Log.print(Log.l.trace, "Binding wireup page complete, now load data");
+                return that.loadTextData();
+            }).then(function () {
+                return that.loadData();
+            }).then(function () {
+                Log.print(Log.l.trace, "Data loaded");
+            });
+            Log.ret(Log.l.trace);
+        })
+    });
+})();
---
+++ pages/overview/overviewService.js
@@ -0,0 +1,74 @@
+﻿// service for page: start
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("Overview", {
+        _textView: {
+            get: function () {
+                return AppData.getFormatView("LangMandantDokument", 20628);
+            }
+        },
+        //MandantDokumentVIEW_20633
+        _medienView: {
+            get: function () {
+                return AppData.getFormatView("MandantDokument", 20635);
+            }
+        },
+        textView: {
+            select: function (complete, error, eventId) {
+                Log.call(Log.l.trace, "eventView.");
+                var restriction = {
+                    VeranstaltungID: null,
+                    DokVerwendungID: 1,
+                    LanguageSpecID: AppData.getLanguageId()
+                };
+                var ret = Overview._textView.select(complete, error, restriction, {
+                    ordered: true,
+                    orderAttribute: "Sortierung"
+                });
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+        },
+        medienView: {
+            select: function (complete, error, eventId) {
+                Log.call(Log.l.trace, "eventView.");
+                var restriction = {
+                    VeranstaltungID: eventId
+                    //LanguageSpecID: AppData.getLanguageId()
+                };
+                var ret = Overview._medienView.select(complete, error, restriction, {
+                    ordered: true
+                    //orderAttribute: "Sortierung"
+                });
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+        },
+        _eventView: {
+            get: function () {
+                return AppData.getFormatView("Veranstaltung", 20647); //selektiere aus andere view
+            }
+        },
+        eventView: {
+            select: function (complete, error, restriction) {
+                Log.call(Log.l.trace, "eventView.");
+                var ret = Overview._eventView.select(complete, error, restriction, {
+                    ordered: true,
+                    orderAttribute: "Startdatum"
+                });
+                Log.ret(Log.l.trace);
+                return ret;
+            }/*,
+            defaultValue: {
+                Name: "",
+                Titel: "",
+                Untertitel: ""
+            }*/
+        }
+    });
+})();
