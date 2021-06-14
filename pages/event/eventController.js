3d34955aa7fe2439bc54a788db332425c4d439c8
Hung <quanghungbui1@gmail.com>
2021-06-14 16:22:28 +0200

    !! bug show hide ev text

--- pages/event/eventController.js
+++ pages/event/eventController.js
@@ -29,7 +29,7 @@
                 showConference: false,
                 showRecordedContent: false,
                 showLogOffEventMail: false,
-                showEvText: false,
+                showEvText: true,
                 showOffText: false,
                 registerStatus: "",
                 //showRegisterConfirm: false,
