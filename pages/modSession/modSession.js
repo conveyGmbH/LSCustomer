56e9ff91b4e47f3e660a53fd3ac6650782df1e10
Gilbert Schwaab <gilbert@convey.de>
2021-06-20 01:48:11 +0200

    recordedContent height

--- pages/modSession/modSession.js
+++ pages/modSession/modSession.js
@@ -118,8 +118,12 @@
                                                     headerHeight = stickyHeader.clientHeight;
                                                 }
                                             }
-                                            //var contentHeight = Math.max(Math.min(height - headerHeight,height - offsetTop),400);
-                                            var contentHeight = Math.max(height - (offsetTop + headerHeight),400);
+                                            var contentHeight;
+                                            if (WinJS.Utilities.hasClass(content, "recordedContent-fragmenthost")) {
+                                                contentHeight = Math.max(height - (offsetTop + headerHeight), 400);
+                                            } else {
+                                                contentHeight = Math.max(Math.min(height - headerHeight,height - offsetTop),400);
+                                            }
                                             content.style.height = contentHeight.toString() + "px";
                                             var fragmentElement = content.firstElementChild;
                                             if (fragmentElement) {
