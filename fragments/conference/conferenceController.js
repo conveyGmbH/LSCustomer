5b5b048e75fdb7f630c27e2e969b2a130f89c93e
Gilbert Schwaab <gilbert@convey.de>
2021-11-11 23:31:17 +0100

    Update conferenceController.js

--- fragments/conference/conferenceController.js
+++ fragments/conference/conferenceController.js
@@ -569,8 +569,7 @@ var __meteor_runtime_config__;
                         if (pos > 0) {
                             html5Client = html5Client.substr(0, pos) +
                                 ";__meteor_runtime_config__.ROOT_URL = \"" + abs("/html5client") + "\"" +
-                                //";__meteor_runtime_config__.ROOT_URL_PATH_PREFIX = \"" + abs("/html5client") + "\"" +
-                                ";__meteor_runtime_config__.ROOT_URL_PATH_PREFIX = \"/html5client\"" +
+                                ";__meteor_runtime_config__.ROOT_URL_PATH_PREFIX = \"" + abs("/html5client") + "\"" +
                                 html5Client.substr(pos);
 
                         }
