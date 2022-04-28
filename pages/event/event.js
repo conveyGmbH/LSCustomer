23ba7516069a416b82b2bd5889c2afbbd28a1711
Hung <quanghungbui1@gmail.com>
2022-04-28 12:36:43 +0200

    fix max height of recorded content

--- pages/event/event.js
+++ pages/event/event.js
@@ -125,9 +125,9 @@
                                             }
                                             var contentHeight;
                                             if (WinJS.Utilities.hasClass(content, "recordedContent-fragmenthost")) {
-                                                contentHeight = Math.max(height - (offsetTop + headerHeight), 400);
+                                                contentHeight = Math.max(height - (offsetTop + headerHeight), 750);
                                             } else {
-                                                contentHeight = Math.max(Math.min(height - headerHeight,height - offsetTop),400);
+                                                contentHeight = Math.max(Math.min(height - headerHeight,height - offsetTop), 750);
                                             }
                                             content.style.height = contentHeight.toString() + "px";
                                             var fragmentElement = content.firstElementChild;
