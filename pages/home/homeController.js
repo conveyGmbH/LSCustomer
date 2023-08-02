9c22a4f7c9b96cb3b3a3a137d700406cadbfd984
Gilbert Schwaab <gilbert@convey.de>
2023-08-02 13:42:55 +0200

    Update homeController.js

--- pages/home/homeController.js
+++ pages/home/homeController.js
@@ -1242,7 +1242,7 @@
                             }
                             var stickyHeaderPinnedFixed = headerHost.firstElementChild.querySelector(".sticky-header-pinned-fixed");
                             if (stickyHeaderPinnedFixed) {
-                                if (listView.winControl.scrollPosition > prevScrollPosition) {
+                                if (listView.winControl.scrollPosition > Math.max(prevScrollPosition,15)) {
                                     if (!WinJS.Utilities.hasClass(stickyHeaderPinnedFixed, "headerup")) {
                                         WinJS.Utilities.addClass(stickyHeaderPinnedFixed, "headerup");
                                     }
