5bf6b521007534d1b2e2b36f303cbd0d4ca170ed
Hung <quanghungbui1@gmail.com>
2025-01-13 16:54:04 +0100

    new style

---
+++ extra/eb52414d/scripts/startup.js
@@ -0,0 +1,47 @@
+/* startup script for customizations */
+(function () {
+    "use strict";
+
+    function saveBodyContent() {
+        var customerElement = document.querySelector("#"+rootElementId);
+        if (customerElement && customerElement.parentElement) {
+            var customerRootElement = customerElement;
+            while (customerRootElement.parentElement && customerRootElement.parentElement !== document.body) {
+                customerRootElement = customerRootElement.parentElement;
+            }
+            // save customer page content
+            var bodyContentTop = document.createElement("DIV");
+            bodyContentTop.setAttribute("class", "saved-body-content-top");
+            var bodyContentBottom = document.createElement("DIV");
+            bodyContentBottom.setAttribute("class", "saved-body-content-bottom");
+            bodyContentBottom.setAttribute("style", "visibility: hidden");
+            var nextBodyChild;
+            var curBodyChild = document.body.firstElementChild;
+            while (curBodyChild && curBodyChild !== customerRootElement) {
+                nextBodyChild = curBodyChild.nextElementSibling;
+                if (curBodyChild.tagName && 
+                    curBodyChild.tagName.toLowerCase() !== "script" &&
+                    curBodyChild.tagName.toLowerCase() !== "link") {
+                    bodyContentTop.appendChild(curBodyChild);
+                }
+                curBodyChild = nextBodyChild;
+            }
+            curBodyChild = customerRootElement.nextElementSibling;
+            while (curBodyChild) {
+                nextBodyChild = curBodyChild.nextElementSibling;
+                if (curBodyChild.tagName && 
+                    curBodyChild.tagName.toLowerCase() !== "script" &&
+                    curBodyChild.tagName.toLowerCase() !== "link") {
+                    bodyContentBottom.appendChild(curBodyChild);
+                }
+                curBodyChild = nextBodyChild;
+            }
+            document.body.insertBefore(bodyContentTop, customerRootElement);
+            document.body.appendChild(bodyContentBottom);
+            bodyContentTop.appendChild(customerRootElement);
+        }
+    }
+
+    saveBodyContent();
+
+})();
